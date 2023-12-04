0f9bb45eefb7d80004582a39c40112db4179645c
morris <147575434+DCodeMorris@users.noreply.github.com>
2023-12-04 12:57:58 -0500

    Update AnimalShelter_CRUD.py

--- CS-499 Algorithms and Data Structures/Artifact Two Release/AnimalShelter_CRUD.py
+++ CS-499 Algorithms and Data Structures/Artifact Two Release/AnimalShelter_CRUD.py
@@ -1,3 +1,36 @@
+# **AnimalShelter_CRUD.py** #
+```python
+# =============================================================================
+# Created By  : Dustin Morris
+# Created Date: Mon November 20 2023
+# =============================================================================
+# Interpreter: Python 3.12
+# File Name: AnimalShelter_CRUD.py
+# =============================================================================
+__course__ = 'CS499'
+__author__ = 'Dustin Morris'
+__version__ = '1.4'
+__maintainer__ = 'Dustin Morris'
+__username__ = 'MyUserAdmins2'
+__password__ = '123456'
+__email__ = 'Dustin.Morris1@snhu.edu'
+__status__ = 'Production'
+__description__ = 'Uses а Dаsh аррliсаtion to mаnаge user рet ԁаtа from аnimаl shelters.'
+# =============================================================================
+print('# ' + '=' * 78)
+print('Author: ' + __author__)
+print('Version: ' + __version__)
+print('Maintainer: ' + __maintainer__)
+print('Email: ' + __email__)
+print('Status: ' + __status__)
+print('Course: ' + __course__)
+print('Username: ' + __username__)
+print('Password: ' + __password__)
+print('Description: ' + __description__)
+print('# ' + '=' * 78)
+
+
+
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import urllib.parse
@@ -54,6 +87,7 @@ class AnimalShelter(object):
         if not query:
             raise Exception("No search criteria is present.")
         else:
+            ```
             _delete_valid = self.dataBase.animals.delete_many(query)
             self.records_deleted = _delete_valid.deleted_count
             return _delete_valid.deleted_count > 0
